66435dd537956a9f60dd747f0f8f65e547fd971f
poncho29 <meneses0828@gmail.com>
2024-12-01 21:50:45 -0500

    add: serve static images

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('api');
 
@@ -13,6 +15,8 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.useStaticAssets(join(__dirname, '../public'));
 
   await app.listen(4000);
 }
